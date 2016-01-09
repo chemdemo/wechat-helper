aebace1a95d11ae5362257918ee4e8435f60c374
chemdemo <yangdemo@gmail.com>
2016-01-09 11:20:45 +0800

    fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /*
 * @Author: dmyang
 * @Date:   2016-01-06 11:41:18
-* @Last Modified by:   dmyang
-* @Last Modified time: 2016-01-08 18:44:45
+* @Last Modified by:   dm
+* @Last Modified time: 2016-01-09 02:42:20
 */
 
 'use strict';
@@ -16,11 +16,17 @@ const util = require('util')
 
 const open = require('open')
 const _ = require('lodash')
+const c = require('colors')
 
+const DEBUD = !!process.argv[2] || 1
 const QR_IMAGE_PATH = './qr.jpg'
 const DEVICE_ID = 'e000000000000000'
 
-let tip = 0
+let _tip = 0
+let _ticket = ''
+let _skey = ''
+let _baseRequest = {}
+let _profile
 
 function get(u, params, preProcess) {
     let arg = arguments
@@ -33,7 +39,7 @@ function get(u, params, preProcess) {
 
     u += '?' + qs.stringify(params, null, null, {encodeURIComponent: (v) => {return v}})
 
-    console.log(`GET ${u}`)
+    if(DEBUD) console.log(c.cyan('GET') + ` ${u}`)
 
     return new Promise((resolve, reject) => {
         (/^https/.test(u) ? https : http).get(u, (res) => {
@@ -66,15 +72,15 @@ function post(u, data, headers) {
 
     let options = {
         method: 'POST',
-        port: 80,
+        // port: 80,
         hostname: parsed.hostname,
-        path: parsed.path,
+        path: parsed.path + '&_t=' + Date.now(),
         headers: _.assign(headers || {}, {
             'Content-Length': postData.length
         })
     }
 
-    console.log(`POST ${u} data ${postData}`)
+    if(DEBUD) console.log(c.cyan('POST') + ` ${u} data ${postData}`)
 
     return new Promise((resolve, reject) => {
         let req = (/^https/.test(options.protocol) ? https : http).request(options, (res) => {
@@ -131,13 +137,13 @@ function genQR(uuid) {
 
     return new Promise((resolve, reject) => {
         get(u, params).then((data) => {
-            tip = 1
+            _tip = 1
             fs.writeFile(QR_IMAGE_PATH, data, (err) => {
                 if(err) reject(err)
                 else resolve(QR_IMAGE_PATH)
             })
         }, (err) => {
-            tip = 1
+            _tip = 1
             reject(err)
         })
     })
@@ -147,7 +153,7 @@ function scanLogin(uuid) {
     let u = 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login'
 
     let params = {
-        tip: tip,
+        tip: _tip,
         uuid: uuid
     }
 
@@ -162,7 +168,7 @@ function scanLogin(uuid) {
 
             process.emit('login:success', ru)
         } else {
-            if(code == 201) tip = 0
+            if(code == 201) _tip = 0
 
             process.emit('login:fail', uuid, code)
         }
@@ -194,42 +200,126 @@ function getBaseInfo(u) {
             let wxuin = m3 ? m3[1] : ''
             let passTicket = m4 ? m4[1] : ''
 
+            _ticket = passTicket
+            _skey = skey
+            _baseRequest = {
+                Uin: parseInt(wxuin),
+                Sid: wxsid,
+                Skey: skey,
+                DeviceID: DEVICE_ID
+            }
+
             // resolve() 只能传一个参数？
             resolve({skey: skey, wxsid: wxsid, wxuin: wxuin, passTicket: passTicket})
         }, reject)
     })
 }
 
-function initWX(baseUrl, info) {
-    let u = baseUrl + 'webwxinit'
+// initwx
+function getProfile(baseUrl) {
+    let u = baseUrl + '/webwxinit'
 
     let params = {
-        pass_ticket: info.passTicket,
-        skey: info.skey
+        pass_ticket: _ticket,
+        skey: _skey
     }
 
     let data = {
-        BaseRequest: {
-            Uin: parseInt(info.wxuin),
-            Sid: info.wxsid,
-            Skey: info.skey,
-            DeviceID: DEVICE_ID,
-        }
+        BaseRequest: _baseRequest
     }
 
     u += '?' + qs.stringify(params, null, null, {encodeURIComponent: (v) => {return v}})
 
-    return post(u, data, {'Content-Type': 'application/json;'})
+    return new Promise((resolve, reject) => {
+        post(u, data, {'Content-Type': 'application/json;'})
+            .then((data) => {
+                data = JSON.parse(data.toString())
+
+                let ret = data.BaseResponse.Ret
+
+                // 个人信息
+                _profile = data.User
+
+                if(ret == 0) resolve(_profile)
+                else reject(Error(DEBUD ? data.BaseResponse.ErrMsg : '初始化微信失败'))
+            }, reject)
+    })
+}
+
+function getContact(baseUrl) {
+    let u = baseUrl + '/webwxgetcontact'
+
+    let params = {
+        pass_ticket: _ticket,
+        skey: _skey
+    }
+
+    let q = qs.stringify(params, null, null, {encodeURIComponent: (v) => {return v}})
+
+    return new Promise((resolve, reject) => {
+        let req = https.request({
+            host: 'wx2.qq.com',
+            hostname: 'wx2.qq.com',
+            path: '/cgi-bin/mmwebwx-bin/webwxgetcontact?' + q,
+            method: 'GET',
+        }, (res) => {
+            let chunks = []
+
+            res.on('data', (chunk) => {
+                chunks.push(chunk)
+            })
+
+            res.on('end', () => {
+                resolve(Buffer.concat(chunks).toString())
+            })
+
+            res.on('error', (err) => {
+                console.error(`response ${u} data ${postData} error`, err)
+                reject(err)
+            })
+        })
+
+        req.on('error', reject)
+
+        req.end()
+
+        return;
+
+        get(u, params).then((data) => {
+            data = JSON.parse(data.toString())
+
+            let ret = data.BaseResponse.Ret
+            let errMsg = data.BaseResponse.ErrMsg
+
+            if(ret == 0) resolve(data)
+            else reject(DEBUD && errMsg ? errMsg: '获取联系人列表失败')
+        }, reject)
+    })
 }
 
 process.on('login:success', (redirectUri) => {
-    let baseUrl = redirectUri.slice(0, redirectUri.lastIndexOf('/')) + '/'
+    let baseUrl = redirectUri.slice(0, redirectUri.lastIndexOf('/'))
 
+    // @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    // // Executes the method call but an alternative way (1) to handle Promise Reject case
+    // $http(mdnAPI)
+    //   .get(payload)
+    //   .then(callback.success, callback.error);
+
+    // // Executes the method call but an alternative way (2) to handle Promise Reject case
+    // $http(mdnAPI)
+    //   .get(payload)
+    //   .then(callback.success)
+    //   .then(undefined, callback.error);
     getBaseInfo(redirectUri)
-        .then(initWX.bind(null, baseUrl), console.error)
-        .then((data) => {
-            console.log(data.toString())
-        }, console.error)
+        .then((info) => {
+            return Promise.all([getProfile(baseUrl), getContact(baseUrl)])
+        })
+        .then((x) => {
+            console.log('next =>', x)
+        }, (err) => {
+            console.error('next err =>', err)
+        })
 
     fs.unlinkSync(QR_IMAGE_PATH)
 })
